84dec6ac63954a44e28f1ee92bf76697c3f0957a
edu.2ddd@gmail.com <edu.2ddd@gmail.com>
2025-03-12 19:52:35 -0300

    cumple

--- tiy_pag98.py
+++ tiy_pag98.py
@@ -1,2 +1,6 @@
 dicc = {'name':'Eduardo', 'surname':'Duarte', 'age':'37', 'city':'Encarnación'}
 print(dicc['name'])
+
+
+fav_num = {'1':'Edu', '2':'Ada', '3':'Sebas', '4':'Shirley', '5':'Benito'}
+print(fav_num['3'])
