9e9053a24cb72ef305bcbf2a836a81979f29ff23
Edu <edu.2ddd@gmail.com>
2025-04-09 15:17:08 -0300

    test file

--- tiy_pag98.py
+++ tiy_pag98.py
@@ -2,5 +2,5 @@ dicc = {'name':'Eduardo', 'surname':'Duarte', 'age':'37', 'city':'Encarnación'}
 print(dicc['name'])
 
 
-fav_num = {'1':'Edu', '2':'Ada', '3':'Sebas', '4':'Shirley', '5':'Benito'}
-print(fav_num['3'])
+fav_num = {'edu':55, 'ada':3, 'sebas':1, 'shirley':27, 'benito':5}
+print(fav_num['edu'])
