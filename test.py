180ef9c06c3a69d04dbd6f888f4114e6b0d8616d
edu.2ddd@gmail.com <edu.2ddd@gmail.com>
2025-06-05 04:29:18 -0300

    tercer ipynb

--- test.py
+++ test.py
@@ -1,28 +1,12 @@
-#Creamos un lista vacia para almacenar aliens
-aliens = []
+prompt = '\nEscribe algo para que el loro repita'
+prompt += '\nIngresa "Salir" para finalizar el programa'
 
-#Creamos 30 aliens
-for alien_number in range(30):
-    new_alien = {'color':'green', 'speed':'slow', 'points':5}
-    aliens.append(new_alien)
+active = True
 
-#Cambiar color, velocidad y puntos
-for alien in aliens[0:3]:
-    if alien['color'] == 'green':
-        alien['color'] = 'yellow'
-        alien['speed'] = 'medium'
-        alien['points'] = 10
+while active:
+    mensaje = input(prompt)
 
-for alien in aliens[:2]:   
-    if alien['color'] == 'yellow':
-        alien['color'] = 'red'
-        alien['speed'] = 'fast'
-        alien['points'] = 15
-
-#Mostramos los primeros 5 aliens
-for alien in aliens[:5]:
-    print(alien)
-print('...')
-
-#Contamos el total de aliens creados en la lista
-print(f'Total aliens en lista aliens {len(aliens)}')
+if mensaje == 'salir':
+    active = False
+else:
+    print(mensaje)
